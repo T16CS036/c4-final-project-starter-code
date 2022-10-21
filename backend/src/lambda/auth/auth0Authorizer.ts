c4e9f17c73feaa955d9305a8b1949bdedd62a81d
MinhDTH <dinhthaihoangminh@gmail.com>
2022-10-21 13:54:00 +0900

    update config to auth

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -14,7 +14,7 @@ const logger = createLogger('auth')
 // to verify JWT token signature.
 // To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
 // DONE
-const jwksUrl = 'https://dev-o6ulw-27.us.auth0.com/.well-known/jwks.json'
+const jwksUrl = 'https://dev-fw6kvb8076nshdcm.us.auth0.com/.well-known/jwks.json'
 
 const client = new JwksClient({
   jwksUri: jwksUrl
